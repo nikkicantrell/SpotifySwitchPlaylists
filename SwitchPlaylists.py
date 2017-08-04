264794b9ed15144bc4bbae76813cd7a1bd4b5338
Nicole Cantrell <nikkicantrell@Nicoles-MacBook-Air.local>
2017-08-04 00:46:49 -0400

    The app now switches songs from one playlist to another

--- SwitchPlaylists.py
+++ SwitchPlaylists.py
@@ -14,14 +14,15 @@ class window(QWidget):
         self.openWindow()
         self.trackBox = None
         self.scroll = None
-        self.displaySongs()
+        submitButton = QPushButton('Switch Songs')
+        submitButton.clicked.connect(self.switchSongs)
+        self.layout.addWidget(submitButton, 0, 2)
     def openWindow(self):
         scope = 'playlist-modify-public'
         token = util.prompt_for_user_token(self.username, scope, self.client_id, self.client_secret, self.redirect_url)
         if token == None:
             pass
             #TODO user auth failed
-        print ('worked')
         self.user = spotipy.Spotify(auth=token)
         self.playlists = self.user.current_user_playlists()
         self.fromRadioButtons = []
@@ -36,13 +37,14 @@ class window(QWidget):
         fromBoxLay = QVBoxLayout()
         while playlists:
             for i, playlist in enumerate(playlists['items']):
-                toRadio = QRadioButton(playlist['name'])
-                toBoxLay.addWidget(toRadio)
-                self.toRadioButtons.append([toRadio, playlist['id']])
-                fromRadio = QRadioButton(playlist['name'])
-                fromRadio.toggled.connect(lambda: self.displaySongs())
-                fromBoxLay.addWidget(fromRadio)
-                self.fromRadioButtons.append([fromRadio, playlist['id']])
+                if playlist['owner']['id'] == self.username:
+                    toRadio = QRadioButton(playlist['name'])
+                    toBoxLay.addWidget(toRadio)
+                    self.toRadioButtons.append([toRadio, playlist['id']])
+                    fromRadio = QRadioButton(playlist['name'])
+                    fromRadio.toggled.connect(lambda: self.displaySongs())
+                    fromBoxLay.addWidget(fromRadio)
+                    self.fromRadioButtons.append([fromRadio, playlist['id']])
             if playlists['next']:
                 playlists = self.user.next(playlists)
             else:
@@ -55,32 +57,30 @@ class window(QWidget):
         if self.trackBox:
             self.layout.removeWidget(self.trackBox)
             self.trackBox.setParent(None)
+            self.trackBox = None
         if self.scroll:
             self.layout.removeWidget(self.scroll)
             self.scroll.setParent(None)
+            self.scroll = None
         self.trackBox = QGroupBox("Songs: ")
-        self.layout.addWidget(self.trackBox, 1, 0)
+        self.layout.addWidget(self.trackBox, 1, 0, 1, 2)
         trackBoxLay = QVBoxLayout()
-        fromId = None
-        toId = None
+        self.fromId = None
+        self.toId = None
         songs = False
         for butt in self.fromRadioButtons:
             if butt[0].isChecked():
-                fromId = butt[1]
+                self.fromId = butt[1]
                 break
-        for butt in self.toRadioButtons:
-            if butt[0].isChecked():
-                toId = butt[1]
-                break
-        tracks = self.user.user_playlist(self.username, fromId)['tracks']
-        self.trackRadioButtons = []
+        tracks = self.user.user_playlist(self.username, self.fromId)['tracks']
+        self.trackCheckBoxes = []
         while tracks:
             for i, item in enumerate(tracks['items']):
                 songs = True
                 track = item['track']
-                trackRadio = QRadioButton(track['name'] + ' by ' + track['artists'][0]['name'])
-                trackBoxLay.addWidget(trackRadio)
-                self.trackRadioButtons.append([trackRadio, track['id']])
+                trackCheck = QCheckBox(track['name'] + ' by ' + track['artists'][0]['name'])
+                trackBoxLay.addWidget(trackCheck)
+                self.trackCheckBoxes.append([trackCheck, track['id']])
             if tracks['next']:
                 tracks = self.user.next(tracks)
             else:
@@ -89,7 +89,22 @@ class window(QWidget):
         if songs:
             self.scroll = QScrollArea()
             self.scroll.setWidget(self.trackBox)
+            self.scroll.setFixedHeight(300)
+            self.scroll.setFixedWidth(400)
             self.layout.addWidget(self.scroll)
+    def switchSongs(self):
+        for butt in self.toRadioButtons:
+            if butt[0].isChecked():
+                self.toId = butt[1]
+                break
+        songIds = []
+        for butt in self.trackCheckBoxes:
+            if butt[0].isChecked():
+                songIds.append(butt[1])
+        self.user.user_playlist_add_tracks(self.username, self.toId, songIds)
+        self.user.user_playlist_remove_all_occurrences_of_tracks(self.username, self.fromId, songIds)
+        self.displaySongs()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
